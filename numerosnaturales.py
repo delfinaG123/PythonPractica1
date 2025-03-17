34f8ee13003527c17efddff469b9475f02543807
delfinaG123 <dauphineg2024@gmail.com>
2025-03-16 21:29:52 -0300

    Add files via upload

---
+++ numerosnaturales.py
@@ -0,0 +1,4 @@
+suma=0
+for i in [1,100]:
+    suma=suma+i
+print("La suma es " + str(suma))
---
+++ programa_temperaturas.py
@@ -0,0 +1,5 @@
+temp_celsius=int(input('Ingresá una temperatura en grados Celsius: '))
+temp_fahrenheit=int(temp_celsius+32)
+print("La temperatura en grados Celsius es " + str(temp_celsius) + " y en grados Fahrenheit es " + str(temp_fahrenheit))
+
+
